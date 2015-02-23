266034927376145f83bcb852a3802c1be0307038
Namdak Tonpa <maxim@synrc.com>
2015-02-23 19:27:58 +0200

    fix build

--- src/smp_js.erl
+++ src/smp_js.erl
@@ -3,8 +3,5 @@
 -compile(export_all).
 -jsmacro([auth/3]).
 
->  iolist_to_binary(smp_js:auth("'maxim'","'123'","'12'")).
-<<"ws.send(enc(tuple(atom('Auth'),bin('maxim'),bin('123'),'12')));\n">>
-
 auth(Username,Token,Services) ->
     ws:send(enc(tuple(atom("Auth"),bin(Username),bin(Token),Services))).
