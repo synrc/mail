878e91b2ddb10f1abb14c008e197733fcdb37639
Namdak Tonpa <maxim@synrc.com>
2017-05-04 17:30:03 +0300

    roster

---
+++ package.exs
@@ -0,0 +1,27 @@
+defmodule ROSTER.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :roster,
+     version: "3.4.0",
+     description: "ROSTER Messaging Protocol",
+     package: package,
+     deps: deps]
+  end
+
+  def application do
+    [mod: {:roster, []}]
+  end
+
+  defp package do
+    [files: ["include", "priv", "src", "LICENSE", "README.md", "rebar.config", "sys.config", "vm.args"],
+     licenses: ["MIT"],
+     maintainers: ["Oleg Zinchenko", "Namdak Tonpa"],
+     name: :roster,
+     links: %{"GitHub" => "https://github.com/synrc/roster"}]
+  end
+
+  defp deps do
+     [{:ex_doc, ">= 0.0.0", only: :dev}]
+  end
+end
