4e56aac75e6a37f09c6b9c132e102ab1bb8f1b09
Namdak Tonpa <maxim@synrc.com>
2019-04-15 17:45:19 +0300

    Message req

--- include/message.hrl
+++ include/message.hrl
@@ -1,56 +1,32 @@
 -ifndef(MESSAGE_HRL).
 -define(MESSAGE_HRL, true).
+-include("feature.hrl").
 
 -type container()     :: chain | cur.
 -type messageType()   :: sys | reply | forward | read | edited | cursor.
 -type messageStatus() :: masync | mdelete | mclear| mupdate | medit.
 -type historyType()   :: updated | get | update | last_loaded | last_msg | get_reply.
--type jobType()       ::  init | update | delete | pending | stop | complete.
 
 -record(muc,            {name = [] :: [] | binary() }).
 -record(p2p,            {from = [] :: [] | binary(),
                          to   = [] :: [] | binary() }).
+-record('Ack',          {id    = [] :: [] | integer(),
+                         table = [] :: [] | atom() }).
 
 -record('Message',      {id        = [] :: [] | integer(),
-                         container = chain :: container(),
-                         feed_id   = [] :: #muc{} | #p2p{},
-                         prev      = [] :: [] | integer(),
-                         next      = [] :: [] | integer(),
-                         msg_id    = [] :: [] | binary(),
+                         client_id = [] :: [] | binary(),
                          from      = [] :: [] | binary(),
                          to        = [] :: [] | binary(),
-                         created   = [] :: [] | integer(),
                          files     = [] :: list(#'File'{}),
                          type      = [] :: messageType(),
                          link      = [] :: [] | integer() | #'Message'{},
                          mgs_status= [] :: messageStatus()}).
 
--record('History',      {roster_id = [] :: [] | binary(),
-                         feed      = [] :: [] | #p2p{} | #muc{},
+-record('History',      {feed      = [] :: [] | #p2p{} | #muc{},
                          size      = 0  :: [] | integer(),
                          entity_id = 0  :: [] | integer(),
                          data      = [] :: integer(),
                          status    = [] :: historyType()}).
 
--record(act,           {name= <<"publish">> :: [] | binary(),
-                        data=[]:: binary() | integer() | list(term())}).
-
--record(messageEvent, { name       = [] :: [] | atom(),
-                        payload    = [] :: binary(),
-                        timeout    = [] :: {integer(),{integer(),integer(),integer()}},
-                        module     = [] :: atom()}).
-
--record('Job',          {id        = [] :: [] | integer(),
-                         container = chain :: container(),
-                         feed_id   = [] :: #act{},
-                         prev      = [] :: [] | integer(),
-                         next      = [] :: [] | integer(),
-                         context   = [] :: [] | integer() | binary(),
-                         proc      = [] :: [] | integer(),
-                         time      = [] :: integer(),
-                         data      = [] :: list( #'Message'{}),
-                         events    = [] :: list(#messageEvent{}),
-                         settings  = [] :: list(#'Feature'{}),
-                         status    = [] ::  jobType()}).
 
 -endif.
--- src/ws/n2o_node.erl
+++ src/ws/n2o_node.erl
@@ -1,22 +1,40 @@
 -module(n2o_node).
+-include("message.hrl").
 -include_lib("n2o/include/n2o.hrl").
 -copyright('Vladimir Kirillov').
 -compile(export_all).
 
-info({text,<<"N2O,",A/binary>>},R,S) -> n2o:reg({client,A}), {reply,{text,<<"USER ",A/binary>>},R,S};
-info({text,<<"MSG ",C/binary>>},R,S) -> {reply, {text, <<"ACK ",C/binary>>},R,S};
-info({flush,Text},R,S)               -> {reply, {text, Text},R,S};
-info(Msg, R,S)                       -> io:format("WS: ~p~n",[Msg]), {unknown,Msg,R,S}.
+info({text,<<"N2O,",A/binary>>},R,S) ->
+   n2o:reg({client,A}),
+   {reply,{text,<<"USER ",A/binary>>},R,S#cx{session = A}};
+
+info({text,<<"MSG ",C/binary>>},R,#cx{session = Sid}=S) ->
+   _Tokens = string:tokens(binary_to_list(C)," "),
+   Msg = #'Message'{client_id=kvx:seq([],[]),files=[#'File'{payload=C}]},
+   {ring,N} = n2o_ring:lookup(Msg),
+   {ok,Res} = n2o:send({server,N},{publish,self(),Sid,Msg}),
+   Delivered = list_to_binary(io_lib:format("~p",[Res])),
+   {reply, {text, <<"ACK ",Delivered/binary>>},R,S};
+
+info(#'Message'{}=M, _, #cx{node=Server}) ->
+    io:format("NODE ~p: ~p~n",[Server,M]),
+   ok;
+
+info({flush,Text},R,S) ->
+   {reply, {text, Text},R,S};
+
+info(Msg, R,S) ->
+   io:format("WS: ~p~n",[Msg]),
+   {unknown,Msg,R,S}.
 
 send(C,M) -> C ! M.
 
 proc(init,#pi{name=Name}=Async) -> n2o:reg(Name), {ok,Async#pi{state=[]}};
 
-proc({publish, C, Server, Token, Request}, State  = #pi{name=Server,state=C}) ->
-    Bert   = n2o:decode(Request),
-    Ctx    = #cx { session= n2o:to_binary(Token), client_pid=C, from=C },
+proc({publish, C, Token, Request}, State = #pi{name=Server}) ->
+    Ctx    = #cx { session= n2o:to_binary(Token), node=Server, client_pid=C, from=C },
     put(context, Ctx),
-    Return = try case n2o_proto:info(Bert,[],Ctx) of
+    Return = try case n2o_proto:info(Request,[],Ctx) of
              {reply,{_,      <<>>},_,_} -> skip;
              {reply,{bert,   Term},_,_} -> {ok,send(C,n2o_bert:encode(Term))};
              {reply,{json,   Term},_,_} -> {ok,send(C,n2o_json:encode(Term))};
@@ -24,9 +42,9 @@ proc({publish, C, Server, Token, Request}, State  = #pi{name=Server,state=C}) ->
              {reply,{default,Term},_,_} -> {ok,send(C,n2o:encode(Term))};
              {reply,{Encoder,Term},_,_} -> {ok,send(C,Encoder:encode(Term))};
                                   Reply -> {error,{"Invalid Return",Reply}} end
-    catch Err:Rea ->
-          n2o:error(?MODULE,"Catch:~p~n",[n2o:stack_trace(Err,Rea)]) end,
+    catch _:_:S -> io:format("Catch:~p~n",[S]) end,
     {reply, Return, State};
 
-proc(Unknown,#pi{name=Name}=Async) -> io:format("NODE ~p: ~p~n",[Name,Unknown]),
+proc(Unknown,#pi{name=Name}=Async) ->
+    io:format("UNKNOWN ~p: ~p~n",[Name,Unknown]),
     {reply,{uknown,Unknown,0},Async}.
