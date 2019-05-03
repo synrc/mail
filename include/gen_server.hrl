d07a5446419189e1e33c57e19f180ea1d2c8832f
Namdak Tonpa <maxim@synrc.com>
2019-05-03 14:21:47 +0300

    Delete gen_server.hrl

--- include/gen_server.hrl
+++
@@ -1,8 +0,0 @@
--ifndef(SMP_HRL).
--define(SMP_HRL, true).
-
--define(GEN_SERVER, [start_link/1,init/1,handle_call/3,handle_cast/2,
-                     handle_info/2,terminate/2,code_change/3]).
-
--endif.
-
